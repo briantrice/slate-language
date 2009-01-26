aaddcb0b31785ef7a97200f511ac529e62db7c54
Guillermo Adrian Molina <guille@chaca-mobile.(none)>
2009-01-26 17:23:37 +0100

    Some unfinished staff

--- src/plugins/glib-wrapper.c
+++ src/plugins/glib-wrapper.c
@@ -127,7 +127,12 @@ EXPORT void wrapper_g_object_connect_to_block_id( gpointer instance, char* aSign
 	g_closure_add_finalize_notifier( closure, (gpointer)blockID, callback_finalize_notifier );
 	g_signal_connect_closure(instance, aSignalName, closure, FALSE);
 }
-
+/*
+EXPORT void wrapper_g_closure_set_handlers( gpointer closure ) {
+	g_closure_set_marshal( closure, callback_marshal);
+	g_closure_add_finalize_notifier( closure, (gpointer)blockID, callback_finalize_notifier );
+}
+*/
 EXPORT gchar *wraper_g_pointer_as_string( gpointer *pointer ) {
 	return (gchar *)pointer;
 }
