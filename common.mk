5c3479a687cd2c3445c6ce41480c975102d1ab2a
Guillermo Adrian Molina <guille@chaca-mobile.(none)>
2009-02-05 17:57:37 +0100

    More LLVM functionality

--- common.mk
+++ common.mk
@@ -97,6 +97,8 @@ CFLAGS_gdk-wrapper.c=`pkg-config --cflags gdk-2.0`
 LDFLAGS_gdk-wrapper.lo=`pkg-config --libs gdk-2.0` `pkg-config --libs gthread-2.0`
 CFLAGS_gtk-wrapper.c=`pkg-config --cflags gtk+-2.0`
 LDFLAGS_gtk-wrapper.lo=`pkg-config --libs gtk+-2.0`
+CFLAGS_llvm.c=`llvm-config --cflags`
+LDFLAGS_llvm.lo=`lvm-config --libs`
 
 #CFLAGS_socket.c=
 
@@ -175,7 +177,7 @@ endif
 
 ifeq ($(HOST_SYSTEM), Linux)
   LIBS       := -lm -ldl
-  PLUGINS    += gtk-wrapper.so gdk-wrapper.so glib-wrapper.so
+  PLUGINS    += gtk-wrapper.so gdk-wrapper.so glib-wrapper.so llvm.so
 endif
 
 ifeq ($(findstring MINGW,$(HOST_SYSTEM)), MINGW)
---
+++ src/plugins/llvm.c
@@ -0,0 +1,25 @@
+#ifdef WIN32
+#  define EXPORT __declspec(dllexport)
+#else
+#  define EXPORT
+#endif
+
+#include <llvm-c/Core.h>
+#include <llvm-c/Analysis.h>
+#include <llvm-c/BitReader.h>
+#include <llvm-c/BitWriter.h>
+#include <llvm-c/ExecutionEngine.h>
+#include <llvm-c/Target.h>
+#include "llvm/Config/config.h"
+
+EXPORT LLVMModuleRef llvm_module_create_with_name(char *moduleID) {
+	return LLVMModuleCreateWithName(moduleID);
+}
+
+EXPORT void llvm_dispose_module(LLVMModuleRef module) {
+	LLVMDisposeModule(module);
+}
+
+EXPORT void llvm_dump_module(LLVMModuleRef module) {
+	LLVMDumpModule(module);
+}
