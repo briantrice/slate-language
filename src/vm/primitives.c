2e58078b8a643668fdaa52fe524fd6d8476c1ed9
Brian T. Rice <briantrice@gmail.com>
2009-04-21 11:49:54 -0700

    Added a "daemonize" primitive based on toy examples from Wikipedia.

--- src/vm/primitives.c
+++ src/vm/primitives.c
@@ -1,4 +1,9 @@
 #include "slate.h"
+#ifdef SLATE_DAEMONIZE
+#include <pwd.h>
+#include <sys/stat.h>
+#include <signal.h>
+#endif
 
 void prim_fixme(struct object_heap* oh, struct Object* args[], word_t n, struct OopArray* opts, word_t resultStackPointer) {
 
@@ -78,7 +83,7 @@ void prim_writeToPipe(struct object_heap* oh, struct Object* args[], word_t arit
 
 }
 
-/*fixme this is a copy of the last function with only the select call changed*/
+/*FIXME this is a copy of the last function with only the select call changed*/
 void prim_selectOnWritePipesFor(struct object_heap* oh, struct Object* args[], word_t arity, struct OopArray* opts, word_t resultStackPointer) {
 
   GC_VOLATILE struct OopArray* selectOn = (struct OopArray*) args[0];
@@ -124,7 +129,7 @@ void prim_cloneSystem(struct object_heap* oh, struct Object* args[], word_t arit
   GC_VOLATILE struct OopArray* array;
 
   /* make two pipes that we can use exclusively in each process to talk to the other */
-  /*fixme remap fds for safety*/
+  /*FIXME remap fds for safety*/
   if (socketpair(AF_UNIX, SOCK_STREAM, 0, pipes) == -1) {
     oh->cached.interpreter->stack->elements[resultStackPointer] = oh->cached.nil;
     return;
@@ -150,6 +155,128 @@ void prim_cloneSystem(struct object_heap* oh, struct Object* args[], word_t arit
 
 #endif
 }
+
+/*
+void prim_cloneSystemInProcess(struct object_heap* oh, struct Object* args[], word_t arity, struct OopArray* opts, word_t resultStackPointer) {
+#ifdef WIN32
+#pragma message("TODO WIN32 port cloning/threading a system")
+#else
+
+#endif
+}
+*/
+
+#ifdef SLATE_DAEMONIZE
+
+/* Change this to whatever your daemon is called */
+#define DAEMON_NAME "slatedaemon"
+   
+/* Change this to the user under which to run */
+#define RUN_AS_USER "root"
+
+static void child_handler(int signum) {
+  switch(signum) {
+  case SIGALRM: exit(EXIT_FAILURE); break;
+  case SIGUSR1: exit(EXIT_SUCCESS); break;
+  case SIGCHLD: exit(EXIT_FAILURE); break;
+  }
+}
+
+void prim_daemonizeSystem(struct object_heap* oh, struct Object* args[], word_t arity, struct OopArray* opts, word_t resultStackPointer) {
+  const char* lock_filename = (char*)byte_array_elements((struct ByteArray*) args[1]);
+#ifdef WIN32
+#pragma message("TODO WIN32 port daemonizing a system")
+#else
+  pid_t pid, sid, parent;
+  int lfp = -1;
+
+  /* already a daemon */
+  if (getppid() == 1) return;
+
+  /* Create the lock file as the current user */
+  if (lock_filename && lock_filename[0]) {
+    lfp = open(lock_filename,O_RDWR|O_CREAT,0640);
+    if (lfp < 0) {
+      printf("Unable to create lock file %s, code=%d (%s)",
+	     lock_filename, errno, strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  /* Drop user if there is one, and we were run as root */
+  if (getuid() == 0 || geteuid() == 0) {
+    struct passwd *pw = getpwnam(RUN_AS_USER);
+    if (pw) {
+      if (!oh->quiet)
+	printf("Setting user to " RUN_AS_USER);
+      setuid(pw->pw_uid);
+    }
+  }
+       
+  /* Trap signals that we expect to receive */
+  signal(SIGCHLD,child_handler);
+  signal(SIGUSR1,child_handler);
+  signal(SIGALRM,child_handler);
+       
+  /* Fork off the parent process */
+  pid = fork();
+  if (pid < 0) {
+    printf("Unable to fork daemon, code=%d (%s)",
+	   errno, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+  /* If we got a good PID, then we can exit the parent process. */
+  if (pid > 0) {
+           
+    /* Wait for confirmation from the child via SIGTERM or SIGCHLD, or
+       for two seconds to elapse (SIGALRM).  pause() should not return. */
+    alarm(2);
+    pause();
+           
+    exit(EXIT_FAILURE);
+  }
+       
+  /* At this point we are executing as the child process */
+  parent = getppid();
+       
+  /* Cancel certain signals */
+  signal(SIGCHLD,SIG_DFL); /* A child process dies */
+  signal(SIGTSTP,SIG_IGN); /* Various TTY signals */
+  signal(SIGTTOU,SIG_IGN);
+  signal(SIGTTIN,SIG_IGN);
+  signal(SIGHUP, SIG_IGN); /* Ignore hangup signal */
+  signal(SIGTERM,SIG_DFL); /* Die on SIGTERM */
+       
+  /* Change the file mode mask */
+  umask(0);
+       
+  /* Create a new SID for the child process */
+  sid = setsid();
+  if (sid < 0) {
+    printf("Unable to create a new session, code %d (%s)",
+	   errno, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+       
+  /* Change the current working directory.  This prevents the current
+     directory from being locked; hence not being able to remove it. */
+  if ((chdir("/")) < 0) {
+    printf("Unable to change directory to %s, code %d (%s)",
+	   "/", errno, strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  /* Redirect standard files to /dev/null */
+  freopen("/dev/null", "r", stdin);
+  freopen("/dev/null", "w", stdout);
+  freopen("/dev/null", "w", stderr);
+
+  /* Tell the parent process that we are A-okay */
+  kill(parent, SIGUSR1);
+#endif
+}
+
+#endif //SLATE_DAEMONIZE
 
 void prim_socketCreate(struct object_heap* oh, struct Object* args[], word_t arity, struct OopArray* opts, word_t resultStackPointer) {
   word_t domain = object_to_smallint(args[0]);
