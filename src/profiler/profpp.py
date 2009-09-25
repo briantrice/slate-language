ea03d01a3f980a720fe35670031abd5d457d408f
timmy <timmy@timmy-desktop.(none)>
2009-09-24 22:42:23 -0400

    profiler fixes. outputs dot file directly now

--- src/profiler/profpp.py
+++ src/profiler/profpp.py
@@ -22,16 +22,34 @@ def readCalls(callsFilename):
     cumTime = {}
     childTime = {}
     callStack = []
+    callStackTime = []
     childCallCount = {}
     lastTime = 0
     lastDepth = None
     inFunction = None
+    progressCounter = 0
     while True:
         entry = fd.read(3 * wordSize)
         if (entry == ''):
             break
         time, depth, method = struct.unpack(structFormat, entry)
         timeDiff = time - lastTime
+        progressCounter += timeDiff
+        if progressCounter > 1000000:
+            sys.stderr.write("Processed a second of time... at " + str(time/1000000) + " seconds\n")
+            progressCounter -= 1000000
+#            sys.stderr.write(str(len(selfTime)))
+#            sys.stderr.write("\n")
+#            sys.stderr.write(str(len(cumTime)))
+#            sys.stderr.write("\n")
+#            sys.stderr.write(str(len(childTime)))
+#            sys.stderr.write("\n")
+#            sys.stderr.write(str(len(childCallCount)))
+#            sys.stderr.write("\n")
+#            sys.stderr.write(str(len(callCount)))
+#            sys.stderr.write("\n")
+#            sys.stderr.write(str(len(callStack)))
+#            sys.stderr.write("\n")
 
         #call count
         incrementEntry(callCount, method, 1)
@@ -43,24 +61,26 @@ def readCalls(callsFilename):
             #child call Count
             if len(callStack) >= 1:
                 parent = callStack[-1]
+                parentTime = callStackTime[-1]
                 if parent not in childCallCount:
                     childCallCount[parent] = {}
                 incrementEntry(childCallCount[parent], method, 1)
             callStack.append(method)
+            callStackTime.append(time)
         else:
-            callStack.pop()
-        for entry in set(callStack): #fixme? double/wrong counting possible even with set?
-            incrementEntry(cumTime, entry, timeDiff)
+            if len(callStack) > 1:
+                child = callStack.pop()
+                startTime = callStackTime.pop()
+                if child not in callStack: # don't double count?
+                    incrementEntry(cumTime, child, time - startTime)
+                if len(callStack) > 1:
+                    parent = callStack[-1]
+                    if parent not in childTime:
+                        childTime[parent] = {}
+                    incrementEntry(childTime[parent], child, time - startTime)
+
             
 
-        #fixme recursive functions
-        #child time
-        for i in range(len(callStack)-1):
-            parent = callStack[i]
-            child = callStack[i+1]
-            if parent not in childTime:
-                childTime[parent] = {}
-            incrementEntry(childTime[parent], child, timeDiff)
                 
         lastTime = time
         lastDepth = depth
@@ -80,12 +100,17 @@ def readKeys(keysFilename):
     fd.close()
     return dict
 
+def methodColor(percent):
+    return "#000000"
+
 def main(args):
-    if len(args) != 2:
-        print("USAGE: " + sys.argv[0] + " <profcalls.out> <profkeys.out>")
+    if len(args) != 3:
+        print("USAGE: " + sys.argv[0] + " <profcalls.out> <profkeys.out> <max-count>")
         return
     callsFilename = args[0]
     keysFilename = args[1]
+    maxCount = int(args[2])
+    maxCountDict = {}
     keys = readKeys(keysFilename)
 #    print(str(keys))
     selfTime, cumTime, childTime, childCallCount, callCount, lastTime = readCalls(callsFilename)
@@ -95,6 +120,9 @@ def main(args):
 #    print(str(childCallCount))
 #    print(str(callCount))
 #    print(str(sorted([(val, key) for key, val in cumTime.items()], key = lambda (x,y): x, reverse = True)))
+    print("digraph {\ngraph [ranksep=0.25, fontname=Arial, nodesep=0.125];\n"
+          "node [fontname=Arial, style=\"filled,rounded\", height=0, width=0, shape=box, fontcolor=white];\n"
+          "edge [fontname=Arial];\n")
 
     for parentMethod, childMethods in childCallCount.items():
         for method, count in childMethods.items():
@@ -102,26 +130,43 @@ def main(args):
                 keys[method] = '???'
 
     functionOrder = sorted([(val, key) for key, val in cumTime.items()], key = lambda (x,y): x, reverse = True)
-    print "index % time    self  children    called     name"
-    index = 1
+    #print "index % time    self  children    called     name"
+    index = 0
     for time, method in functionOrder:
-        #childCumTime = [val for key, val in childCou]
-        print('[' + str(index) + '] '+ str(cumTime[method] * 100.0 / lastTime) \
-                  + ' ' + str(selfTime[method] * 100.0 / lastTime) \
-                  + ' ?? ' \
-                  + ' ' + str(callCount[method]) \
-                  + ' ' + str(keys[method]))
+        maxCountDict[method] = True
+        if (index > maxCount):
+            break
+        index += 1
+
+    for time, method in functionOrder:
+        selfPercent = round(selfTime[method]*100.0/lastTime, 2)
+        cumPercent = round(cumTime[method]*100.0/lastTime, 2)
+        if method not in maxCountDict:
+            continue
+        print(str(method) + " [color=\"" + methodColor(cumPercent)+ "\", fontcolor=\"#ffffff\", fontsize=\"10.00\", "
+              "label=\"" + str(keys[method]) + "\\n" + str(cumPercent) + "%\\n(" + str(selfPercent) + "%)\\n" +
+              str(callCount[method]) + "\"]")
         if method in childTime:
             childTimes = childTime[method]
-            childTimeSorted = sorted([(val, key) for key, val in childTimes.items()], key = lambda (x,y): x, reverse = True)
-            for childFullTime, childMethod in childTimeSorted:
-                print((' '*8)+ str(childFullTime * 100.0 / lastTime) \
-                          + ' ??  ?? ' \
-                          + ' ' + str(childCallCount[method][childMethod]) + '/' + str(callCount[childMethod]) \
-                          + ' ' + str(keys[childMethod]))
+            fullChildTime = 0
+            for childMethod, childFullTime in childTimes.items():
+                fullChildTime += childFullTime
+            
+            for childMethod, childFullTime in childTimes.items():
+                if childMethod not in maxCountDict:
+                    continue
+                childPercentTime = round(childFullTime * 100.0 / fullChildTime, 2)
+                if childMethod not in childCallCount[method]:
+                    cCount = '0'
+                else:
+                    cCount = str(childCallCount[method][childMethod])
+                print(str(method) + " -> " + str(childMethod) +
+                      " [color=\"" + methodColor(childPercentTime)+ "\", fontcolor=\"#000000\", fontsize=\"10.00\", "
+                      "penwidth=\"1.0\", label=\"" + str(childPercentTime) + "%\\n" + cCount + "\"]")
 
 
-        index = index + 1
+
+    print "\n}\n"
     #for method in [[key, val] for key, val in selfTime.items()].sort(key = lambda obj: obj[1]):
     #print(str(keys[method]))
 
